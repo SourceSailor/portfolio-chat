1013ce29bdb1b2ef3c6d9c1f230e16ef15fb8954
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2026-08-05 20:45:23 -0700

    Chat Interface now animates on component mount and exit

--- src/components/chat/ChatInterface.jsx
+++ src/components/chat/ChatInterface.jsx
@@ -5,7 +5,7 @@ import ChatIntro from "../ChatIntro";
 import SuggestedButton from "../SuggestedButton";
 import { suggestedBtnText } from "../../../config";
 
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 
 const ChatInterface = ({ chatMessages, sendMessageToAI, isLoading }) => {
   const textAreaRef = useRef(null);
@@ -22,8 +22,18 @@ const ChatInterface = ({ chatMessages, sendMessageToAI, isLoading }) => {
     <section
       className={`max-w-3xl w-full mx-auto flex flex-col h-full px-4 sm:px-6 md:px-10 gap-6 sm:gap-10 relative justify-center`}
     >
-      {!hasMessage && <ChatIntro />}
-
+      <AnimatePresence>
+        {!hasMessage && (
+          <motion.div
+            layout
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0, transition: { duration: 0.5 } }}
+            exit={{ opacity: 0.1, transition: { duration: 0.1 } }}
+          >
+            <ChatIntro />
+          </motion.div>
+        )}
+      </AnimatePresence>
       {hasMessage && (
         <ChatMessages chatMessages={chatMessages} isLoading={isLoading} />
       )}
