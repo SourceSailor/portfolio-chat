3709c69cd13dfecee614f1a008685da681d294de
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2026-08-14 15:09:19 -0700

    Update to chat messages

--- src/components/chat/ChatMessages.jsx
+++ src/components/chat/ChatMessages.jsx
@@ -5,7 +5,7 @@ const ChatMessages = ({ chatMessages, isLoading }) => {
   const lastMessage = chatMessages[chatMessages.length - 1];
   const isWaitingForReply = isLoading && lastMessage?.role === "user";
   return (
-    <div className="flex flex-1 flex-col w-full mt-20 gap-10 ">
+    <div className="flex flex-1 flex-col w-full mt-20 gap-10 overflow-auto">
       {chatMessages.map((msg) => (
         <MessageBubble key={msg.id} msg={msg} />
       ))}
