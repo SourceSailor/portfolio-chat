0e7837c990e6da404fc8d3bf01ab8652680bfb31
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2026-08-15 17:37:15 -0700

    Remove user name text from header on md size screens

--- src/components/chat/ChatHeader.jsx
+++ src/components/chat/ChatHeader.jsx
@@ -6,9 +6,9 @@ const ICONSIZE = "w-4";
 const ChatHeader = ({ clearChat }) => {
   return (
     <header className="flex justify-center gap-2 py-4 font-mono-display">
-      <div className="flex flex-1 justify-end md:justify-between px-4 sm:px-6 md:px-10">
-        <div className={`hidden md:flex items-center ${TEXTSIZE}`}>
-          <p>Kyle Pickard |</p>
+      <div className="flex flex-1 justify-end sm:justify-between px-4 sm:px-6 md:px-10">
+        <div className={`hidden sm:flex items-center ${TEXTSIZE}`}>
+          <p className="hidden md:flex">Kyle Pickard |</p>
           <div className="w-2 h-2 rounded-full bg-green-500 animate-pulse mx-1" />
           <p>Accepting new commits</p>
         </div>
